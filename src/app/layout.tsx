0bceea5dab9565d04bd208d27fefc4d13eec60a4
TheGamingmatic <120349595+TheGamingmatic@users.noreply.github.com>
2025-03-20 13:38:22 -0400

    pop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Theme>
+          {/* <Header /> */}
+          {children}
+      </Theme>
         {children}
       </body>
     </html>
---
+++ src/app/pages/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { Theme } from "@radix-ui/themes";
+
+export default function () {
+	return (
+		<html>
+			<body>
+				<Theme>
+				</Theme>
+			</body>
+		</html>
+	);
+}
