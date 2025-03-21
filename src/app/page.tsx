075148130aba6a21a382dd1d838cde2a5db3484f
TheGamingmatic <120349595+TheGamingmatic@users.noreply.github.com>
2025-03-20 21:35:35 -0400

    More updates on main page
    Cleaning up buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,36 @@ import { Container, Flex, Button } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen bg-gray-100">
       <main>
-        <Container size="2">
+        <Container size="2" className="bg-black min-h-screen flex flex-col items-top justify-top p-4">
+          <br></br>
+          <br></br>
           <Flex direction={"column"} align="center" justify="center" gap="2">
-            <h1 className="text-4xl flex-auto font-bold maximumimpact">Welcome to Home Explained!</h1>
-            <p className="text-1xl flex-auto" align="center">Home Explained is a project orientated towards teaching users about our world.
+            <h1 className="text-4xl flex-auto font-bold maximumimpact text-gray-500">Welcome to Home Explained!</h1>
+            <p className="text-1xl flex-auto text-gray-500" align="center">Home Explained is a project orientated towards teaching users about our world.
                 It includes <b> english, math, science, geography, and history.</b> 
                 The project allows users to learn these topics via fun interactive games.
                 Users are allowed to learn not only through interactive games but also through articles, videos, and books.</p>
           </Flex>
       <br></br>
-          <Flex className="" align="center" justify="center" gap="1">
-            <Button variant="soft" color="gray" size="4" radius="large">
-              Sign-in
+      <br></br>
+          <Flex direction={"column"} align="center" justify="center" gap="2">
+            <Button className="flex-auto" variant="solid" color="gray" size="4" radius="large" >
+              Get Started
             </Button>
-            <Button variant="solid" color="dark-blue" size="4" radius="large">
-              Sign-up
-            </Button>
+          <br></br>
           </Flex>
+          <Container>
+            <Flex className="" align="center" justify="center" gap="3">
+              <Button color="slate" variant="soft" size="3" radius="large">
+                Sign-in
+              </Button>
+              <Button variant="solid" color="indigo" size="3" radius="large">
+                Sign-up
+              </Button>
+            </Flex>
+          </Container>
         </Container>
       </main>
     </div>
